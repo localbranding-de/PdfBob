861153cff6800cd45e9ca04a3c96ee13cce598e4
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2021-08-24 09:53:59 +0200

    Update Plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -25,7 +25,7 @@ class Plugin implements BundlePluginInterface
     {
         return [
             BundleConfig::create(PdfbobBundle::class)
-             ->setLoadAfter([ContaoCoreBundle::class),
+             ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
 }
