ecb5f42f9016b7d635d5a411821a1b3365c89bdb
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2021-08-24 09:14:03 +0200

    Update and rename MembershiplevelsBundleTest.php to PdfbobBundleTest.php

--- tests/PdfbobBundleTest.php
+++ tests/PdfbobBundleTest.php
@@ -8,17 +8,17 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace LocalbrandingDe\ExtendedProductDetailBundle\Tests;
+namespace LocalbrandingDe\PdfbobBundle\Tests;
 
 use LocalbrandingDe\ExtendedProductDetailBundle\ExtendedProductDetailBundle;
 use PHPUnit\Framework\TestCase;
 
-class MembershiplevelsBundleTest extends TestCase
+class PdfbobBundleTest extends TestCase
 {
     public function testCanBeInstantiated()
     {
         $bundle = new ExtendedProductDetailBundle();
 
-        $this->assertInstanceOf('Localbranding-de\ExtendedProductDetailBundle\ExtendedProductDetailBundle', $bundle);
+        $this->assertInstanceOf('Localbranding-de\PdfbobBundle\PdfbobBundle', $bundle);
     }
 }
